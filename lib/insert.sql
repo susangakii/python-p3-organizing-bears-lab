cd28e5ab3c5f3bdb315757b7329b6ba9a25f5503
Susan Gakii <susangakii71@gmail.com>
2025-11-26 19:39:20 +0300

    adds sql queries

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 20, 'M', 'brown', 'calm', 1),
+('Rowdy', 15, 'M', 'black', 'aggressive', 1),
+('Tabitha', 8, 'F', 'tan', 'sweet', 1),
+('Sergeant Brown', 18, 'M', 'dark brown', 'strict', 1),
+('Melissa', 12, 'F', 'white', 'gentle', 1),
+('Grinch', 3, 'M', 'greenish', 'grumpy', 1),
+('Wendy', 7, 'F', 'brown', 'friendly', 1),
+(NULL, 5, 'M', 'grey', 'mysterious', 0);
